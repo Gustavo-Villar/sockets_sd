f4d9d128628c891135b9784ceb6cbdca60e5e2ee
Gabriel Gava <nero.gava@gmail.com>
2020-05-28 20:27:09 -0300

    Integral + Master + Slave

--- slave.c
+++ slave.c
@@ -1,8 +1,60 @@
-#include "stdio.h"
-#include "stdlib.h"
+#include <stdio.h> 
+#include <sys/socket.h> 
+#include <arpa/inet.h> 
+#include <unistd.h> 
+#include <string.h> 
 
-int main(int argc, char const *argv[])
+#define PORT 8080 
+
+// Function to calculate the integral
+double integral(double gap, double discretization)
 {
-  puts("Slave");
-  return 0;
+
+  // Trapeze Rule:
+  // h = (b - a) / N
+
+  double total = 0;
+  double xa = 0;
+  double xb = 0;
+
+  xa = sqrt((100 * 100) - (gap * gap));
+  xb = sqrt((100 * 100) - ((gap + discretization) * (gap + discretization)));
+
+  total = ((xa + xb) * discretization) / 2;
+
+  return total;
 }
+
+int main(int argc, char const *argv[]) 
+{ 
+    int sock = 0, valread; 
+    struct sockaddr_in serv_addr; 
+    char *hello = "Hello from client"; 
+    char buffer[1024] = {0}; 
+    if ((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0) 
+    { 
+        printf("\n Socket creation error \n"); 
+        return -1; 
+    } 
+   
+    serv_addr.sin_family = AF_INET; 
+    serv_addr.sin_port = htons(PORT); 
+       
+    // Convert IPv4 and IPv6 addresses from text to binary form 
+    if(inet_pton(AF_INET, "127.0.0.1", &serv_addr.sin_addr)<=0)  
+    { 
+        printf("\nInvalid address/ Address not supported \n"); 
+        return -1; 
+    } 
+   
+    if (connect(sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0) 
+    { 
+        printf("\nConnection Failed \n"); 
+        return -1; 
+    } 
+    send(sock , hello , strlen(hello) , 0 ); 
+    printf("[CLIENT] -> Datagrama enviado: %s\n",hello); 
+    valread = read( sock , buffer, 1024); 
+    printf("[CLIENT] <- Datagrama recebido: %s\n",buffer ); 
+    return 0; 
+} 
